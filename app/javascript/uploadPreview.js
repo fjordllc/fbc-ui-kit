877129eca376e0b21f21fc67ed6aedc6d788157a
teppei machida <machida@fjord.jp>
2020-05-09 00:03:16 +0900

    Update

--- app/javascript/uploadPreview.js
+++ app/javascript/uploadPreview.js
@@ -1,17 +1,18 @@
-window.addEventListener('DOMContentLoaded', function () {
-  const uploader = document.getElementById('js-upload-preview');
-  const preview = document.getElementById('js-upload-preview__preview');
-  if (uploader != null) {
-    uploader.addEventListener("change", function () {
-      // フォームで選択された全ファイルを取得
-      var fileList = this.files;
-      // 個数分の画像を表示する
-      for (var i = 0, l = fileList.length; l > i; i++) {
-        // Blob URLの作成
-        var blobUrl = window.URL.createObjectURL(fileList[i]);
-        // HTMLに書き出し (src属性にblob URLを指定)
-        preview.innerHTML += '<a href="' + blobUrl + '" target="_blank"><img src="' + blobUrl + '"></a>';
-      }
-    });
+window.addEventListener('turbolinks:load', function () {
+  let input = document.querySelector(".js-upload-preview .js-upload-preview__upload")
+  let preview = document.querySelector(".js-upload-preview .js-upload-preview__preview")
+  input.addEventListener("change", (e) => {
+    handleChange(e)
+  })
+
+  function handleChange(event) {
+    while (preview.lastChild) {
+      preview.removeChild(preview.lastChild);
+    }
+    let file = event.target.files[0]
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    var blobUrl = window.URL.createObjectURL(file)
+    preview.innerHTML += '<img src="' + blobUrl + '">'
   }
 }, false);
